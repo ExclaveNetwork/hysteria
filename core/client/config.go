c73b2a0988b11f19c4116176a7c0c37b8fa5d744
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2025-05-25 14:42:41 +0800

    Expose tls.Config and quic.Config

--- core/client/config.go
+++ core/client/config.go
@@ -1,12 +1,13 @@
 package client
 
 import (
-	"crypto/x509"
+	"crypto/tls"
 	"net"
 	"time"
 
 	"github.com/apernet/hysteria/core/v2/errors"
-	"github.com/apernet/hysteria/core/v2/international/pmtud"
+
+	"github.com/apernet/quic-go"
 )
 
 const (
@@ -20,8 +21,8 @@ type Config struct {
 	ConnFactory     ConnFactory
 	ServerAddr      net.Addr
 	Auth            string
-	TLSConfig       TLSConfig
-	QUICConfig      QUICConfig
+	TLSConfig       *tls.Config
+	QUICConfig      *quic.Config
 	BandwidthConfig BandwidthConfig
 	FastOpen        bool
 
@@ -40,38 +41,6 @@ func (c *Config) verifyAndFill() error {
 	if c.ServerAddr == nil {
 		return errors.ConfigError{Field: "ServerAddr", Reason: "must be set"}
 	}
-	if c.QUICConfig.InitialStreamReceiveWindow == 0 {
-		c.QUICConfig.InitialStreamReceiveWindow = defaultStreamReceiveWindow
-	} else if c.QUICConfig.InitialStreamReceiveWindow < 16384 {
-		return errors.ConfigError{Field: "QUICConfig.InitialStreamReceiveWindow", Reason: "must be at least 16384"}
-	}
-	if c.QUICConfig.MaxStreamReceiveWindow == 0 {
-		c.QUICConfig.MaxStreamReceiveWindow = defaultStreamReceiveWindow
-	} else if c.QUICConfig.MaxStreamReceiveWindow < 16384 {
-		return errors.ConfigError{Field: "QUICConfig.MaxStreamReceiveWindow", Reason: "must be at least 16384"}
-	}
-	if c.QUICConfig.InitialConnectionReceiveWindow == 0 {
-		c.QUICConfig.InitialConnectionReceiveWindow = defaultConnReceiveWindow
-	} else if c.QUICConfig.InitialConnectionReceiveWindow < 16384 {
-		return errors.ConfigError{Field: "QUICConfig.InitialConnectionReceiveWindow", Reason: "must be at least 16384"}
-	}
-	if c.QUICConfig.MaxConnectionReceiveWindow == 0 {
-		c.QUICConfig.MaxConnectionReceiveWindow = defaultConnReceiveWindow
-	} else if c.QUICConfig.MaxConnectionReceiveWindow < 16384 {
-		return errors.ConfigError{Field: "QUICConfig.MaxConnectionReceiveWindow", Reason: "must be at least 16384"}
-	}
-	if c.QUICConfig.MaxIdleTimeout == 0 {
-		c.QUICConfig.MaxIdleTimeout = defaultMaxIdleTimeout
-	} else if c.QUICConfig.MaxIdleTimeout < 4*time.Second || c.QUICConfig.MaxIdleTimeout > 120*time.Second {
-		return errors.ConfigError{Field: "QUICConfig.MaxIdleTimeout", Reason: "must be between 4s and 120s"}
-	}
-	if c.QUICConfig.KeepAlivePeriod == 0 {
-		c.QUICConfig.KeepAlivePeriod = defaultKeepAlivePeriod
-	} else if c.QUICConfig.KeepAlivePeriod < 2*time.Second || c.QUICConfig.KeepAlivePeriod > 60*time.Second {
-		return errors.ConfigError{Field: "QUICConfig.KeepAlivePeriod", Reason: "must be between 2s and 60s"}
-	}
-	c.QUICConfig.DisablePathMTUDiscovery = c.QUICConfig.DisablePathMTUDiscovery || pmtud.DisablePathMTUDiscovery
-
 	c.filled = true
 	return nil
 }
@@ -84,25 +53,6 @@ type udpConnFactory struct{}
 
 func (f *udpConnFactory) New(addr net.Addr) (net.PacketConn, error) {
 	return net.ListenUDP("udp", nil)
-}
-
-// TLSConfig contains the TLS configuration fields that we want to expose to the user.
-type TLSConfig struct {
-	ServerName            string
-	InsecureSkipVerify    bool
-	VerifyPeerCertificate func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error
-	RootCAs               *x509.CertPool
-}
-
-// QUICConfig contains the QUIC configuration fields that we want to expose to the user.
-type QUICConfig struct {
-	InitialStreamReceiveWindow     uint64
-	MaxStreamReceiveWindow         uint64
-	InitialConnectionReceiveWindow uint64
-	MaxConnectionReceiveWindow     uint64
-	MaxIdleTimeout                 time.Duration
-	KeepAlivePeriod                time.Duration
-	DisablePathMTUDiscovery        bool // The server may still override this to true on unsupported platforms.
 }
 
 // BandwidthConfig describes the maximum bandwidth that the server can use, in bytes per second.
