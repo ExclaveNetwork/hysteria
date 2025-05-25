d01b6562ab932977d5461595372aba3d1a8080bb
JimmyHuang454 <jimmyhuang454@gmail.com>
2025-05-25 14:25:30 +0800

    Add V2Ray changes

--- core/client/client.go
+++ core/client/client.go
@@ -9,9 +9,9 @@ import (
 	"time"
 
 	coreErrs "github.com/apernet/hysteria/core/v2/errors"
-	"github.com/apernet/hysteria/core/v2/internal/congestion"
-	"github.com/apernet/hysteria/core/v2/internal/protocol"
-	"github.com/apernet/hysteria/core/v2/internal/utils"
+	"github.com/apernet/hysteria/core/v2/international/congestion"
+	"github.com/apernet/hysteria/core/v2/international/protocol"
+	"github.com/apernet/hysteria/core/v2/international/utils"
 
 	"github.com/apernet/quic-go"
 	"github.com/apernet/quic-go/http3"
@@ -26,6 +26,8 @@ type Client interface {
 	TCP(addr string) (net.Conn, error)
 	UDP() (HyUDPConn, error)
 	Close() error
+	OpenStream() (quic.Stream, error)
+	GetQuicConn() quic.Connection
 }
 
 type HyUDPConn interface {
@@ -159,8 +161,8 @@ func (c *clientImpl) connect() (*HandshakeInfo, error) {
 	}, nil
 }
 
-// openStream wraps the stream with QStream, which handles Close() properly
-func (c *clientImpl) openStream() (quic.Stream, error) {
+// OpenStream wraps the stream with QStream, which handles Close() properly
+func (c *clientImpl) OpenStream() (quic.Stream, error) {
 	stream, err := c.conn.OpenStream()
 	if err != nil {
 		return nil, err
@@ -168,8 +170,12 @@ func (c *clientImpl) openStream() (quic.Stream, error) {
 	return &utils.QStream{Stream: stream}, nil
 }
 
+func (c *clientImpl) GetQuicConn() quic.Connection {
+	return c.conn
+}
+
 func (c *clientImpl) TCP(addr string) (net.Conn, error) {
-	stream, err := c.openStream()
+	stream, err := c.OpenStream()
 	if err != nil {
 		return nil, wrapIfConnectionClosed(err)
 	}
